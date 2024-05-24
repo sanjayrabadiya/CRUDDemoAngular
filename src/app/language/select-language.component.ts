c79844f1e3a544951baa74f7e7141850f957e445
projectgsc <project@gsc-technologies.com>
2024-05-24 14:43:07 +0530

    remove space

--- src/app/language/select-language.component.ts
+++ src/app/language/select-language.component.ts
@@ -1,17 +1,13 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
-
 @Component({
-    selector: 'app-select-language-component',
-    templateUrl: './select-language.component.html',
-  })
-
+  selector: 'app-select-language-component',
+  templateUrl: './select-language.component.html',
+})
 export class SelectLanguageComponent {
   newLanguage: string = '';
-
   constructor(public activeModal: NgbActiveModal) { }
-
   addLanguage() {
     if (this.newLanguage.trim() !== '') {
       this.activeModal.close(this.newLanguage.trim());
